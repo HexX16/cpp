ed91c5bb58183f34968a6c1144fc67e2194423c7
hexx <viktorzu6@mail.ru>
2026-04-14 12:53:08 +0300

    14.04.26 binary_search_answer

--- pointers/pointers/pointers.cpp
+++ pointers/pointers/pointers.cpp
@@ -4,19 +4,30 @@ void square(int* x) {
 	*x *= *x;
 }
 
+void change_elem(int* array) {
+	*array = *(array + 1);
+}
+
 int main()
 {
-	int n = 5;
+	/*int n = 5;
 	int* ptr_n = &n;
-	std::cout << ptr_n << std::endl;
+	std::cout << ptr_n << " = " << *ptr_n << std::endl;
 	ptr_n++;
-	std::cout << ptr_n <<"\n" << std::endl;
+	std::cout << ptr_n << " = " << *ptr_n << "\n" << std::endl;
 
-	int array[2]{ 1,2 };
-	std::cout << array << " = " << *array << "\n" << array + 1 << " = " << *(array + 1) << "\n" << std::endl;
+	std::cout << "array_1" << std::endl;
+	int array_1[2]{ 1,2 };
+	std::cout << array_1 << " = " << *array_1 << "\n" << array_1 + 1 << " = " << *(array_1 + 1) << "\n" << std::endl;
 
 	std::cout << "Input n = ";
 	std::cin >> n;
 	square(&n);
-	std::cout << "Square n = " << n << std::endl;
+	std::cout << "Square n = " << n << "\n" << std::endl;*/
+
+	std::cout << "array_2" << std::endl;
+	int array_2[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	std::cout << *array_2 << "\n";
+	change_elem(array_2);
+	std::cout << *array_2;
 }
---
+++ search_algorithms/binary_search_answer/binary_search_answer/binary_search_answer.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+bool can(int x, int array[], int array_size, int k) {
+	int sum = 0;
+	int count = 1;
+	for (int i = 0; i < array_size; i++) {
+		sum += array[i];
+		if (sum > x) {
+			count += 1;
+			sum = array[i];
+		}
+		if (count > k) {
+			std::cout << "Wrong x = " << x << std::endl;
+			return false;
+		}
+	}
+	return true;
+}
+
+int binary_search_answer(int array[], int array_size, int k) {
+	int min_sum = -9999;
+	int max_sum = 0;
+	for (int i = 0; i < array_size; i++) {
+		if (array[i] > min_sum) min_sum = array[i];
+		max_sum += array[i];
+	}
+	std::cout << "min = " << min_sum << "\t" << "max = " << max_sum << std::endl;
+
+	int mid = 0;
+	int l = min_sum;
+	int r = max_sum;
+	while (l < r){
+		mid = (l + r) / 2;
+		if (can(mid, array, array_size, k)) {
+			std::cout << "Correct x = " << mid << std::endl;
+			r = mid;
+		}
+		else {
+			l = mid + 1;
+		}
+	}
+	return l;
+}
+
+int main()
+{
+	int array[9]{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int array_size = sizeof(array) / sizeof(array[0]);
+	int k = 3;
+	binary_search_answer(array, array_size, k);
+}
+
