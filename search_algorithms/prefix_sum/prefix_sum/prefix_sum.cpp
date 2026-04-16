6dec04d8907b655e0ba02b18f0a2552981a3a839
hexx <viktorzu6@mail.ru>
2026-04-16 14:13:02 +0300

    16.04.26

---
+++ search_algorithms/prefix_sum/prefix_sum/prefix_sum.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+void prefix_sum(int array[], int prefix_array[], int array_size) {
+	prefix_array[0] = array[0];
+	for (int i = 1; i < array_size; i++) {
+		prefix_array[i] = prefix_array[i - 1] + array[i];
+	}
+}
+
+void output(int array[], int prefix_array[], int array_size) {
+	for (int i = 0; i < array_size; i++) {
+		std::cout << "index: " << i << "\t" << "array = " << array[i] << "\t" << "prefix_array = " << prefix_array[i] << std::endl;
+	}
+}
+
+
+int main()
+{
+	int array[6] = { 7, 9, 0, 2, 1, 5 };
+	int array_size = sizeof(array) / sizeof(array[0]);
+	int prefix_array[6];
+	prefix_sum(array, prefix_array, array_size);
+	output(array, prefix_array, array_size);
+
+	//example
+	int count_1_4 = prefix_array[4] - prefix_array[0];
+	std::cout << count_1_4;	
+}
---
+++ search_algorithms/sliding_window_variable_size/sliding_window_variable_size/sliding_window_variable_size.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int sliding_window_var_size(int array[], int array_size, int k) {
+	int l = 0;
+	int r = 0;
+	int sum = 0;
+	while (l<r){
+		sum += array[r];
+		if (sum >= k) {
+			l--;
+		}
+		else{
+			r++;
+		}
+	}
+	std::cout << (r - l);
+	return (r - l);
+}
+
+int main()
+{
+	// Найти минимальную длину непрерывного подмассивa, сумма которого ≥ k
+	int array[6]{ 2, 3, 1, 2, 4, 3 };
+	int array_size = sizeof(array) / sizeof(array[0]);
+	int k = 7;
+	sliding_window_var_size(array, array_size, k);
+}
