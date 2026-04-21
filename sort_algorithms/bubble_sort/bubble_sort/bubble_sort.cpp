0d4d733fe7cee8f6fb0def864db217ad2b455994
Viktor <viktorzu6@gmail.com>
2026-04-21 12:26:29 +0300

    21.04.26

--- sort_algorithms/bubble_sort/bubble_sort/bubble_sort.cpp
+++ sort_algorithms/bubble_sort/bubble_sort/bubble_sort.cpp
@@ -2,30 +2,37 @@
 #include <vector>
 #include <random>
 
-void bubble_sort(std::vector<int>&, int);
+void bubble_sort(std::vector<int>&);
 void swap(std::vector<int>&, int, int);
-void output_array(std::vector<int>&);
+void output_array(const std::vector<int>&);
+void gen_array(std::vector<int>&);
 
 int main()
 {
-	std::random_device rd;              // источник энтропии
-	std::mt19937 gen(rd());             // генератор
-	std::uniform_int_distribution<> dist(1, 100); // диапазон
+	
 	const int array_size = 100;
-	std::vector<int> array(array_size);
-	for (int i = 0; i < array_size; i++) {
-		array[i] = dist(gen);
-	}
+	std::vector<int> array(100);
+	gen_array(array);
+
+	std::cout << "before:\t";
 	output_array(array);
-	bubble_sort(array, array_size);
+	bubble_sort(array);
+	std::cout << "\nafter:\t";
 	output_array(array);
 }
 
-void bubble_sort(std::vector<int>& array, int array_size) {
-	for (int i = 0; i < array_size - 2; i++) {
-		if (array[i] > array[i + 1]) {
-			swap(array, i, i + 1);
+void bubble_sort(std::vector<int>& array) {
+	bool flag = true;
+	int size = array.size();
+	while (flag) {
+		flag = false;
+		for (int i = 0; i < size - 1; i++) {
+			if (array[i] > array[i + 1]) {
+				swap(array, i, i + 1);
+				flag = true;
+			}
 		}
+		size--;
 	}
 }
 
@@ -37,6 +44,15 @@ void swap(std::vector<int>& array, int l, int r) {
 
 void output_array(const std::vector<int> &array) {
 	for (int i = 0; i < array.size(); i++) {
-		std::cout << array[i];
+		std::cout << array[i] << " ";
+	}
+}
+
+void gen_array(std::vector<int>& array) {
+	std::random_device rd;              // источник энтропии
+	std::mt19937 gen(rd());             // генератор
+	std::uniform_int_distribution<> dist(1, 100); // диапазон
+	for (int i = 0; i < array.size(); i++) {
+		array[i] = dist(gen);
 	}
 }
---
+++ sort_algorithms/quick_sort/quick_sort/quick_sort.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+
+void quick_sort(const std::vector<int>&);
+void output_array(const std::vector<int>&);
+void gen_array(std::vector<int>&);
+
+int main()
+{
+	const int array_size = 100;
+	std::vector<int> array(array_size);
+	gen_array(array);
+
+	std::cout << "before:\t";
+	output_array(array);
+
+	//std::cout << "\nafter:\t";
+	//output_array(array);
+}
+
+void quick_sort( std::vector<int>& array) {
+
+}
+
+void output_array(const std::vector<int>& array) {
+	for (int i = 0; i < array.size(); i++) {
+		std::cout << array[i] << " ";
+	}
+}
+
+void gen_array(std::vector<int>& array) {
+	std::random_device rd;              // источник энтропии
+	std::mt19937 gen(rd());             // генератор
+	std::uniform_int_distribution<> dist(1, 100); // диапазон
+	for (int i = 0; i < array.size(); i++) {
+		array[i] = dist(gen);
+	}
+}
