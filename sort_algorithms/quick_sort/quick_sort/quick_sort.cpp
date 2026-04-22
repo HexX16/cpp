6d46f7cf1056bdb2a5bb825af9fac378995c98c8
Viktor <viktorzu6@gmail.com>
2026-04-22 15:19:26 +0300

    22.04.26 quick_sort complete

--- sort_algorithms/quick_sort/quick_sort/quick_sort.cpp
+++ sort_algorithms/quick_sort/quick_sort/quick_sort.cpp
@@ -2,9 +2,10 @@
 #include <vector>
 #include <random>
 
-void quick_sort(const std::vector<int>&);
+void quick_sort(std::vector<int>&, int, int);
 void output_array(const std::vector<int>&);
 void gen_array(std::vector<int>&);
+void swap(std::vector<int>&, int, int);
 
 int main()
 {
@@ -14,19 +15,47 @@ int main()
 
 	std::cout << "before:\t";
 	output_array(array);
-
-	//std::cout << "\nafter:\t";
-	//output_array(array);
+	quick_sort(array, 0, array_size-1);
+	std::cout << "\nafter:\t";
+	output_array(array);
 }
 
-void quick_sort( std::vector<int>& array) {
+void quick_sort(std::vector<int>& array, int l, int r) {
+	if (l >= r) return;
 
+	int pivot = array[(l + r) / 2];
+	std::cout << "pivot = " << pivot << std::endl;
+
+	int i = l;
+	int j = r;
+
+	while (i <= j) {
+		while (array[i] < pivot) i++;
+		while (array[j] > pivot) j--;
+
+		if (i <= j) {
+			swap(array, i, j);
+			i++;
+			j--;
+		}
+		std::cout << "l = " << l << "\tr = " << r << std::endl;
+	}
+
+	quick_sort(array, l, j);
+	quick_sort(array, i, r);
+}
+	
+void swap(std::vector<int>& array, int i, int j) {
+	int ch = array[i];
+	array[i] = array[j];
+	array[j] = ch;
 }
 
 void output_array(const std::vector<int>& array) {
 	for (int i = 0; i < array.size(); i++) {
 		std::cout << array[i] << " ";
 	}
+	std::cout << "\n" << std::endl;
 }
 
 void gen_array(std::vector<int>& array) {
@@ -37,3 +66,4 @@ void gen_array(std::vector<int>& array) {
 		array[i] = dist(gen);
 	}
 }
+
